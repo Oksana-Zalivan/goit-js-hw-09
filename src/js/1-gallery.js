e97ca5afdf73927c2927f3ade1944ef46a8a5955
Oksana Zalivan <ksyushamartinovska@gmail.com>
2025-11-07 22:16:57 +0300

    Completed gallery task with SimpleLightbox

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,25 @@
-console.log('Gallery');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { images } from './images.js';
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = images.map(({ preview, original, description }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+            <img class="gallery-image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+`).join('');
+
+galleryEl.innerHTML = markup;
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
